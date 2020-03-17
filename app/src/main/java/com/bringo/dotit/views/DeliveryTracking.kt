7f2c812221416e68d516734cbb1ad6e1097c22cb
Baha Ferchichi <baha.ferchichi@aiesec.net>
2020-03-17 04:12:41 +0100

    fixed map bug, switched to real pics

--- app/src/main/java/com/bringo/dotit/views/DeliveryTracking.kt
+++ app/src/main/java/com/bringo/dotit/views/DeliveryTracking.kt
@@ -3,51 +3,69 @@ package com.bringo.dotit.views
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.bringo.dotit.R
 import com.bringo.dotit.databinding.TrackingBinding
 import com.bringo.dotit.utils.Hell
 import com.bringo.dotit.viewmodels.TrackingViewModel
-import com.google.android.gms.maps.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import java.lang.Exception
+
 
 class DeliveryTracking : Fragment(), OnMapReadyCallback {
 
     lateinit var binding : TrackingBinding
     lateinit var viewModel: TrackingViewModel
     private lateinit var mMap: GoogleMap
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_delivery_tracking, container, false)
         viewModel = ViewModelProviders.of(this).get(TrackingViewModel::class.java)
         binding.categoryviewmodel=viewModel
+        val rootView: View = binding.root
 
         initViewModelListeners()
 
-        return binding.root
-    }
+        val mapFragment =
+            childFragmentManager.findFragmentById(com.bringo.dotit.R.id.mapi) as SupportMapFragment?
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        try {
+        mapFragment!!.getMapAsync { mMap ->
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            mMap.clear() //clear old markers
 
-        }catch (e:Exception){
-            Hell("causeee $e // ${childFragmentManager}" +
-                    "// ${childFragmentManager
-                        .findFragmentById(R.id.mapi)==null}")
+            val googlePlex = CameraPosition.builder()
+                .target(LatLng(35.8376202,10.6314504))
+                .zoom(16f)
+                .bearing(0f)
+                .tilt(45f)
+                .build()
+
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 2000, null)
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(35.8376202,10.6314504))
+                    .title("Iron Man")
+                    .snippet("Dish coming!")
+            )
         }
-        val mapFragment = childFragmentManager
-            .findFragmentById(R.id.mapi) as MapFragment
-        mapFragment.getMapAsync(this)
+
+
+        return rootView
     }
 
     private fun initViewModelListeners() {
@@ -55,16 +73,8 @@ class DeliveryTracking : Fragment(), OnMapReadyCallback {
         viewModel.intitializeInfo(dishName)
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        Hell("map is readyyyyyy")
-        val restau = LatLng(35.8376202,10.6314504)
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.addMarker(MarkerOptions().position(restau).title("Planet food"))
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(restau))
+    override fun onMapReady(googleMap: GoogleMap) {
 
     }
 
